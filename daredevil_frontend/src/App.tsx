9068cb31ec5c424261285728427ceb988be5eed7
woodFordR <adam@kaewell.com>
2025-09-11 01:08:54 -0400

    adding kendo react library components, changing to scss, fixing App.tsx, adding json test data, updating gitignore

--- daredevil_frontend/src/App.tsx
+++ daredevil_frontend/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid, GridColumn } from '@progress/kendo-react-grid'
+import { DropDownList, type DropDownListChangeEvent } from '@progress/kendo-react-dropdowns'
+import React from 'react';
+import './App.scss';
+import products from './products.json'
+import categories from './categories.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [category, setCategory] = React.useState(null)
+
+  const handleDropDownChange = React.useCallback((event: DropDownListChangeEvent) => {
+    setCategory(event.target.value.CategoryID)
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="App">
+      <h1>Daredevil</h1>
+      <Grid data={products}>
+        <GridColumn field="ProductName" />
+        <GridColumn field="UnitPrice" />
+        <GridColumn field="UnitsInStock" />
+        <GridColumn field="Discontinued" />
+      </Grid>
+
+      <h2>Hello KendoReact!</h2>
+      <p>
+        <DropDownList
+          data={categories}
+          dataItemKey="CategoryID"
+          textField="CategoryName"
+          defaultItem={{ CategoryID: null, CategoryName: 'Product categories' }}
+          onChange={handleDropDownChange}
+        />
+        &nbsp; Selected category ID: <strong>{category}</strong>
       </p>
-    </>
+    </div>
   )
 }
 
 export default App
+
--- daredevil_frontend/src/main.tsx
+++ daredevil_frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
-import './index.css'
 import App from './App.tsx'
+import { Calendar } from '@progress/kendo-react-dateinputs'
+import '@progress/kendo-theme-default/dist/all.css'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
+    <Calendar />
   </BrowserRouter>,
 )
