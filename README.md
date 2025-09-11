14279b6850765dfac7a635609da1226841ae2581
woodFordR <adam@kaewell.com>
2025-09-11 00:29:19 -0400

    starting point

---
+++ README.md
@@ -0,0 +1,29 @@
+# Daredevil Backend: uv + FastAPI + Python
+---
+### This part of Daredevil provides a minimal setup to get FastAPI working using uv package/project manager.
+
+ - ```cd daredevil_backend```
+ - ```source .venv/bin/activate```
+ - ```uv run fastapi dev```
+
+ - ```http://127.0.0.1:8000/```
+ - ```http://127.0.0.1:8000/docs```
+
+ - to exit venv ```deactivate```
+
+---
+---
+
+# Daredevil Frontend: React + TypeScript + Vite
+---
+### This part of Daredevil provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+ - ```cd daredevil_frontend```
+ - ```npm install```
+ - ```npm run dev```
+
+ - ```http://localhost:5173/```
+
+---
+---
+
---
+++ daredevil_frontend/eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+])
+
