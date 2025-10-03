ecf015808775ff8622aececd5dd7668a22fcdb3c
woodFordR <adam@kaewell.com>
2025-10-03 11:07:06 -0400

    fixing fetch api call with correct params

--- daredevil_frontend/src/api/repositories.ts
+++ daredevil_frontend/src/api/repositories.ts
@@ -9,9 +9,12 @@ type ApiError =
 
 export const getRepos = (token: string): ResultAsync<Repository[], ApiError> => {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const params = new URLSearchParams({
+    user_token: token
+  });
 
   return ResultAsync.fromPromise(
-    fetch(`${backendUrl}/github/repos?${token}`)
+    fetch(`${backendUrl}/github/repos?${params}`)
       .then(async (response) => {
         if (!response.ok) {
           throw {
