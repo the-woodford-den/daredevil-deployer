a75384be5c3ba6c190a84a1edea0631e5479f6d0
woodFordR <adam@kaewell.com>
2025-11-03 17:08:35 -0500

    setting up user register page

---
+++ daredevil_frontend/src/pages/Register/Register.tsx
@@ -0,0 +1,17 @@
+import { Container, Grid, GridItem, Text } from '@chakra-ui/react';
+
+export function Register() {
+
+  return (
+    <Container>
+      <Grid>
+        <GridItem>
+          <Text>
+            Create Daredevil User
+          </Text>
+        </GridItem>
+      </Grid>
+    </Container>
+  );
+};
+
