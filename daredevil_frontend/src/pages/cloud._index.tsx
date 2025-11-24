c02227247e7c34129cec3e76fe3fac67a1e7bedf
woodFordR <adam@kaewell.com>
2025-11-24 01:15:56 -0500

    fixing login for cloud flow

--- daredevil_frontend/src/pages/cloud._index.tsx
+++ daredevil_frontend/src/pages/cloud._index.tsx
@@ -1,23 +1,13 @@
-import { Outlet, redirect, useLoaderData } from "react-router";
+import { useLoaderData } from "react-router";
 import {
-  Box,
-  GridItem,
-  Mark,
+  Container,
   Text,
+  VStack
 } from '@chakra-ui/react';
-import { userStore } from "@/state";
 import { CloudTree } from "@/components/CloudTree/CloudTree";
-import underUrl from '~/underline.svg';
 
 export async function clientLoader() {
-  const user = userStore.getState();
-  if (!user.username) {
-    console.log(user);
-    throw redirect("/login");
-  }
-
   return {
-    user: user,
     title: 'Just Keep Showing Up & Life Will Reward You',
   };
 }
@@ -26,29 +16,12 @@ export default function Cloud() {
   let data = useLoaderData();
 
   return (
-    <GridItem alignItems="start" colSpan={3}>
-      <Text mt="3" color="aqua" alignItems="right" mb="5" className="t-font">Cloud Console</Text>
-      <CloudTree />
-      <Outlet />
-      <Box>
-        <Mark
-          key={`${data.user.username}1`}
-          css={{
-            fontStyle: 'italic',
-            color: "purple.500",
-            position: "relative",
-          }}
-        >
-          <span>{data.user.username}</span>
-          <img
-            style={{ position: "absolute", left: 0 }}
-            src={underUrl}
-            loading="lazy"
-            alt="line"
-          />
-        </Mark>
-      </Box>
-    </GridItem>
+    <Container width="50%">
+      <VStack>
+        <CloudTree />
+        <Text>{data.title}</Text>
+      </VStack>
+    </Container>
   );
 }
 
--- daredevil_frontend/src/pages/cloud.console.tsx
+++ daredevil_frontend/src/pages/cloud.console.tsx
@@ -1,17 +1,25 @@
 import { Container, Flex, Separator, Stack, Text } from '@chakra-ui/react';
+import { useLoaderData } from "react-router";
+
+export async function clientLoader() {
+  return {
+    title: 'Console',
+  };
+}
 
 export default function Console() {
-
+  let data = useLoaderData();
 
   return (
     <Container direction="column">
-      <Flex align="center" justify="space-between" className="t-font">
+      <Text pb="3" textStyle="4xl">{data.title}</Text>
+      <Flex pt="3" align="center" justify="space-between" className="t-font">
         <Stack>
-          <Text>First</Text>
+          <Text textStyle="2xl">First</Text>
           <Separator />
-          <Text>Second</Text>
+          <Text textStyle="2xl">Second</Text>
           <Separator />
-          <Text>Third</Text>
+          <Text textStyle="2xl">Third</Text>
         </Stack>
       </Flex>
     </Container>
